e3c5c9c9f61d342ac04eb2a0ccf00795a88b1915
ntuf <nibblestuffy@gmail.com>
2019-09-30 02:55:16 +0900

    Initial Commit

---
+++ Hiragana/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hiragana
+//
+//  Created by hiroyuki tomiduka on 2019/09/30.
+//  Copyright © 2019 ntuf. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
